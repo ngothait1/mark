a1982f6cf3d1e506a8479755a240562ddea0acf7
ngothait1 <75209361+ngothait1@users.noreply.github.com>
2024-12-18 10:21:46 +0200

    Add files via upload

--- my_final_project.py
+++ my_final_project.py
@@ -8,7 +8,7 @@ def selectFromMenu():
           "6. Print all entries\n"
           "7. Print entry by index\n"
           "8. Exit")
-    return fetchNumberWithin("Please enter your choice: ", 1, 8)
+    return fetchNumberWithin("Please enter your choice", 1, 8)
 
 # Verify that a number is within a range an return an integer
 def fetchNumberWithin(what_is_fetched, min_value=0, max_value=9999999999): 
@@ -43,25 +43,24 @@ def saveNewEntry(target_database, target_list):
     print("ID [" + str(user_typed_id) + "] saved succesfully")
     return user_typed_age
 
-def searchById(looking_for_id, source_db):
-    for index, key in enumerate(source_db):
-        if key == looking_for_id:
-            printEntry(index, source_db, False)
-            return
-    print(str(looking_for_id) + " could not be found in the database")
+def searchById(id_in_question, source_dict, source_list):
+    if id_in_question in source_list:
+        index_of_id = source_list.index(id_in_question)
+        printUsers(index_of_id, source_dict, source_list, print_as_list=False)
+        return
+    print(str(id_in_question) + " could not be found in the database")
     
-def printEntry(indexed_entry, source_db, print_as_list=True): 
-    for indexed_itteration, key in enumerate(source_db):
-        if indexed_itteration == indexed_entry:
-            spacer = ""
-            if print_as_list:
-                spacer = "    "
-                print(str(indexed_entry)+ ". " + str(key))
-            else:
-                print("ID: " + str(key))
-            print(spacer + "Name: " + source_db[key]["NAME"])
-            print(spacer + "Age: " + str(source_db[key]["AGE"]))      
-            break
+def printUsers(indexed_entry, source_dict, source_list, print_as_list=True): 
+    if indexed_entry < len(source_dict):
+        key = source_list[indexed_entry]
+        spacer = ""
+        if print_as_list:
+            spacer = "    "
+            print(str(indexed_entry)+ ". " + str(key))
+        else:
+            print("ID: " + str(key))
+            print(spacer + "Name: " + source_dict[key]["NAME"])
+            print(spacer + "Age: " + str(source_dict[key]["AGE"]))      
     else:
         return 
     
@@ -77,9 +76,9 @@ def printAllIds(source_list):
     for i, id in enumerate(source_list):
         print(str(i) + ". " +str(id)) 
 
-def printAllEntries(source_db):
-    for i, index in enumerate(source_db):
-        printEntry(i, source_db)
+def printAllEntries(source_dict, source_list):
+    for i, index in enumerate(source_dict):
+        printUsers(i, source_dict, source_list, print_as_list=True)
  
 def requestToExit():
     while True:
@@ -90,6 +89,16 @@ def requestToExit():
         elif y_or_n == "n":
             return False
 
+def printAgesAverage():
+        average_age = sum_of_ages/len(user_database)
+        print(average_age)
+
+def checkDatabaseEmpty():
+    if len(user_database) == 0:
+        print("Error: Can't perform operation - Database is empty")
+    else:
+        return False 
+
 user_database = {}
 user_ids = []
 sum_of_ages = 0 
@@ -99,41 +108,28 @@ while True:
     if choice == 1:
         sum_of_ages = sum_of_ages + saveNewEntry(user_database, user_ids)
     elif choice == 2:
-        if len(user_database) == 0:
-            print("Error: Can't perform search - Database is empty")
-        else:
+        if checkDatabaseEmpty() == False:
             entered_id = fetchNumberWithin(what_is_fetched="ID")
             if entered_id in user_database:
-                searchById(entered_id, user_database)
+                searchById(entered_id, user_database, user_ids)
             else: print("Error: ID " + str(entered_id) + " is not saved")
     elif choice == 3:
-        if len(user_database) == 0:
-            print("Error: There are no users in the database yet.")
-        else:
-            average_age = sum_of_ages/len(user_database)
-            print(average_age)
+        if checkDatabaseEmpty() == False:
+            printAgesAverage()
     elif choice == 4:
-        if len(user_database) == 0:
-            print("Error: There are not names to display yet")
-        else:
+        if checkDatabaseEmpty() == False:
             printAllNames(user_database)
     elif choice == 5:
-        if len(user_ids) == 0:
-            print("Error: There are no ID's to display yet")
-        else:
+        if checkDatabaseEmpty() == False:
             printAllIds(user_ids)
     elif choice == 6:
-        if len(user_database) == 0:
-            print("Error: Database is empty")    
-        else:
-            printAllEntries(user_database)   
+        if checkDatabaseEmpty() == False:
+            printAllEntries(user_database, user_ids)   
     elif choice == 7:
-        if len(user_database) == 0:
-            print("Error: Database is empty")
-        else:
+        if checkDatabaseEmpty() == False:
             typed_index = fetchNumberWithin(what_is_fetched="Index")
             if typed_index < len(user_database):
-                printEntry(typed_index, user_database, False)
+                printUsers(typed_index, user_database, user_ids, print_as_list=False)
             else:
                 print("Index out of range. The maximum index allowed is " + str((len(user_database)-1)))        
     elif choice == 8:
